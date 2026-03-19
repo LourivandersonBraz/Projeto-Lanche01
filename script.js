65fa20b49567ddd6d7a83298c65f253d32c35d37
LourivandersonBraz <braz.eng.software@gmail.com>
2026-03-18 22:10:42 -0300

    Configuração
    Configurando o TailwindCSS

---
+++ script.js
@@ -0,0 +1,91 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const adressInput = document.getElementById("adress")
+const adressWarn = document.getElementById("adress-warn")
+
+let cart = [];
+
+//Abrir Modal Carrinho
+cartBtn.addEventListener("click", function() {
+    cartModal.style.display = "flex"
+})
+
+//Fechar Modal Clicando Fora
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function(){
+        cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCar(name, price)
+       }
+})
+
+// Função Adicionar no Carrinho
+function addToCar(name, price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+        existingItem.quantity += 1;
+        return;
+    }else{
+        cart.push({
+        name,
+        price,
+        quantity: 1,
+    })
+    }
+
+    updateCartModal()
+}
+
+
+//Atualiza Carrinho
+
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+       const cartItemElement = document.createElement("div");
+        
+       cartItemElement.innerHTML = `
+        <div>
+            <div>
+                <p>${item.name}</p>
+                <p>${item.quantity}</p>
+                <p>R$ ${item.price}</p>
+            </div>
+
+            <div>
+                <button>
+                    Remover
+                </button>
+            </div>
+
+        </div>
+
+       `
+        cartItemElement.appendChild(cartItemElement)
+
+       
+    })
+
+}
